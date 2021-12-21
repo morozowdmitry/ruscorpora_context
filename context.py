2881c0ed5ff123a26f0f59f8bdacc0df07a5c5ab
Dmitry <morozov@ruscorpora.ru>
2021-12-21 18:26:38 +0700

    simple context getter

---
+++ context.py
@@ -0,0 +1,37 @@
+import random
+
+import requests
+from urllib.parse import quote
+import json
+
+
+def get_context(lemma):
+    quoted_lemma = quote(lemma)
+    seed = random.randint(0, 100000)
+
+    url = 'https://processing.ruscorpora.ru/search.xml?' \
+          'dpp=1&' \
+          'spd=1&' \
+          'p=0&' \
+          'lang=ru&' \
+          'level1=0&' \
+          f'lex1={quoted_lemma}&' \
+          'mode=main&' \
+          'nodia=1&' \
+          'out=normal&' \
+          f'seed={seed}&' \
+          'sem-mod1=sem&sort=random&' \
+          'text=lexgramm&' \
+          'format=json'
+
+    r = requests.get(url)
+
+    data = json.loads(r.text)
+
+    snippet_words = data['document_groups'][0][0]['snippets'][0]['words']
+
+    return ''.join([x['text'] for x in snippet_words]).strip()
+
+
+if __name__ == "__main__":
+    print(get_context('слово'))
